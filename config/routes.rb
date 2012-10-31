748c333e40d6a399d5f9b45717d8382a328b6140
Tomas Gregor <tomas.gregor@me.com>
2012-10-31 12:50:06 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 KagyuVitamin::Application.routes.draw do
  
-  get '/quotes'           => 'Quotes#index',   :as => 'quotes'
-  post '/quotes'          => 'Quotes#create'
-  get '/quotes/new'       => 'Quotes#new',     :as => 'new'
-  get '/quotes/today'     => 'Quotes#today',   :as => 'today'
+  root :to => '/quotes/today'
+ 
+  get '/quotes'           => 'quotes#index',   :as => 'quotes'
+  post '/quotes'          => 'quotes#create'
+  get '/quotes/new'       => 'quotes#new',     :as => 'new'
+  get '/quotes/today'     => 'quotes#today',   :as => 'today'
   
-  get '/quotes/teacher/:id'    => 'Quotes#teacher',   :as => 'teacher'
+  get '/quotes/teacher/:id'    => 'quotes#teacher',   :as => 'teacher'
   
-  get '/quotes/:id'       => 'Quotes#show',    :as => 'quote'
-  delete '/quotes/:id'    => 'Quotes#destroy'
-  put '/quotes/:id'       => 'Quotes#update'
-  get '/quotes/:id/edit'  => 'Quotes#edit',    :as => 'edit'
+  get '/quotes/:id'       => 'quotes#show',    :as => 'quote'
+  delete '/quotes/:id'    => 'quotes#destroy'
+  put '/quotes/:id'       => 'quotes#update'
+  get '/quotes/:id/edit'  => 'quotes#edit',    :as => 'edit'
   
   
   
